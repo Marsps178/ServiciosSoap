caa8ebdd4ea754655abb422cc382ebfa604c25b1
Haisen <SUITE@SUITEBG301-ST13.cibertec.edu.pe>
2026-05-17 11:42:12 -0500

    Create Project Hilos

---
+++ hilos/src/main/java/cibertec/pe/HiloNumeroLetra.java
@@ -0,0 +1,5 @@
+package cibertec.pe;
+
+public class HiloNumeroLetra {
+
+}
---
+++ hilos/src/main/java/cibertec/pe/Principal.java
@@ -0,0 +1,7 @@
+package cibertec.pe;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
